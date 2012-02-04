4edf5cc3e734d7d4fcedc1175c0c457e294143bd
Puneeth Chaganti <punchagan@gmail.com>
2012-02-04 11:15:20 +0530

    ENH: Get the images with highest resolution possible.
    The feed url doesn't contain the links to images of maximum available
    resolution.  Fix this, by scanning the json-like metadata on the
    album's main page and search for the maximum possible resolution and
    download it.

--- picasa_downloader.py
+++ picasa_downloader.py
@@ -7,9 +7,11 @@ from urllib2 import urlopen
 from os.path import basename, join, exists
 from os import mkdir
 from urlparse import urlsplit
-from BeautifulSoup import BeautifulStoneSoup
+from BeautifulSoup import BeautifulSoup
 import sys
 import time
+import re
+import json
 
 # Based on http://stackoverflow.com/a/3160819
 class ProgressBar(object):
@@ -44,25 +46,65 @@ def get_feed_url(url):
                     url = link['href']
                     return url
 
-def download_photos(url, location):
+def get_photo_urls(url):
+    """ Get individual photo urls and other info, given url of main page.
+    """
+    if urlsplit(url).netloc == "picasaweb.google.com":
+        content = urlopen(url).read()
+        soup = BeautifulSoup(content)
+        for script in soup.findAll('script'):
+            if 'albumCoverUrl' in script.text:
+                break
+        script = ''.join(script.text.splitlines())
+        json_like = re.findall('{.*}', script)[0]
+        F = '"feed":'
+        start, end = json_like.find(F)+len(F), json_like.rfind('}}')
+        pics_dict = json.loads(json_like[start:end])
+        info = [dict(url=pic['media']['content'][0]['url'],
+                     size=pic['size'],
+                     height=pic['height'],
+                     width=pic['width']) for pic in pics_dict['entry']]
+        print 'Found %d pictures' %(len(info),)
+        return info
+
+def get_size_dir_url(url, size):
+    base, pic = url.rsplit('/', 1)
+    return '/'.join([base, 's%s' %(size,), pic])
+
+def download_photos(info, location):
     if not exists(location):
         mkdir(location)
-    feed_content = urlopen(url).read()
-    tags = BeautifulStoneSoup(feed_content).findAll('media:content')
-    count = len(tags)
-    print "%d pictures found!" % count
-    progress_bar = ProgressBar(count)
-    for i, each in enumerate(tags):
-        if each.has_key('url'):
-            img = urlopen(each['url']).read()
-            fname = join(location, basename(urlsplit(each['url'])[2]))
-            output = open(fname,'wb')
-            output.write(img)
-            output.close()
-            progress_bar.update(i)
+    else:
+        print 'Using existing location'
+    progress_bar = ProgressBar(len(info))
+    for i, pic in enumerate(info):
+        url = pic['url']
+        height = pic['height']
+        width = pic['width']
+        size = pic['size']
+
+        # Find the dimension to use for getting largest possible picture.
+        image_fp, max_size = None, 0
+        for dim in (width, height):
+            img_fp = urlopen(get_size_dir_url(url, dim))
+            content_length = img_fp.headers.get('content-length')
+            if content_length > max_size:
+                image_fp = img_fp
+                max_size = content_length
+            if content_length == size:
+                break
+
+        if max_size < size:
+            print "Couldn't get original size for %s" %(url,)
+        fname = join(location, basename(url))
+        output = open(fname,'wb')
+        output.write(image_fp.read())
+        output.close()
+        progress_bar.update(i)
+
 
 if __name__ == '__main__':
     url = raw_input('Enter the Picasa web album/feed url : ')
-    url = get_feed_url(url)
-    name_album = raw_input('Enter name of Album : ')
-    download_photos(url, name_album)
+    info = get_photo_urls(url)
+    name_album = raw_input('Enter a path/directory for the album: ')
+    download_photos(info, name_album)
